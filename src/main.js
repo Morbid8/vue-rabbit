6ff310811f68cc33701146064c4bc1531aea83b1
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-07 18:22:14 +0800

    静态资源的引入

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+//引入初始化样式文件
+import '@/styles/commom.scss'
 //测试接口函数
 import {getCategory} from '@/apis/testAPI'
 getCategory().then(res=>{
