3d122d8241ade667ee1295b61793f804053298f5
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-13 22:52:18 +0800

    Home-面板组件封装

---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,59 @@
+<script setup>
+// 定义props
+// props 是父组件传递给子组件的数据。
+defineProps({
+  // 主标题
+  title: {
+    type: String
+  },
+  // 副标题
+  subTitle: {
+    type: String
+  }
+})
+
+</script>
+
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subTitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+      <!-- slot 是用来 占位显示父组件传进来的内容 的。插槽 -->
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,11 @@ import HomeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="轻松好物" subTitle="测试面板副标题">
+    <div>测试面板内容</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="测试面板副标题2">
+    <div>测试面板内容2</div>
+  </HomePanel>
 </template>
