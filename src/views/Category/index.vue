2f28faba24e7c4c1b6b1474f34b73d36b4b33ce9
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-14 17:53:18 +0800

    一级分类-解决路由缓存问题

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -10,14 +10,22 @@ import {getCategoryAPI} from '@/apis/category'
 import {ref,onMounted} from 'vue'
 import {useRoute} from 'vue-router'
 import { getBannerAPI } from '@/apis/home'
+import {onBeforeRouteUpdate} from 'vue-router'
 
 //获取数据
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
-  const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id=route.params.id) => {
+  const res = await getCategoryAPI(id)
   categoryData.value = res.result
 }
+//目标：路由参数变化的时候，可以把分类数据接口重新发送
+onBeforeRouteUpdate((to)=>{
+  console.log('路由变化了')
+  //存在问题：使用最新的路由参数请求最近的分类数据
+  console.log(to);
+  getCategory(to.params.id)
+})
 
 onMounted(() => getCategory())
 //获取banner
