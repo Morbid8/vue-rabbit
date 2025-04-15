a037339e3b9ed009ae502b7cb364d9c1a6e297d5
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-15 19:28:14 +0800

    登录-请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,8 +2,8 @@
 import axios from 'axios'
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
-// import { useRouter } from 'vue-router'
-// import { useUserStore } from '@/stores/userStore'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/userStore'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout: 5000
@@ -27,26 +27,26 @@ httpInstance.interceptors.response.use(res => res.data, e => {
 })
 
 // axios请求拦截器
-// httpInstance.interceptors.request.use(config => {
-//   // 1. 从pinia获取token数据
-//   const userStore = useUserStore()
-//   // 2. 按照后端的要求拼接token数据
-//   const token = userStore.userInfo.token
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`
-//   }
-//   return config
-// }, e => Promise.reject(e))
+httpInstance.interceptors.request.use(config => {
+  // 1. 从pinia获取token数据
+  const userStore = useUserStore()
+  // 2. 按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, e => Promise.reject(e))
 
-// // axios响应式拦截器
-// httpInstance.interceptors.response.use(res => res.data, e => {
-//   // 统一错误提示
-//   ElMessage({
-//     type: 'warning',
-//     message: e.response.data.message
-//   })
-//   return Promise.reject(e)
-// })
+// axios响应式拦截器
+httpInstance.interceptors.response.use(res => res.data, e => {
+  // 统一错误提示
+  ElMessage({
+    type: 'warning',
+    message: e.response.data.message
+  })
+  return Promise.reject(e)
+})
 
 
 export default httpInstance
