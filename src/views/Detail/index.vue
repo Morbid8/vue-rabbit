2042a8807d30945f4ab09ee08a86902442c62989
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-15 23:31:57 +0800

    本地购物车-添加购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -5,9 +5,9 @@ import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
-// import { ElMessage } from 'element-plus'
-// import { useCartStore } from '@/stores/cartStore'
-// const cartStore = useCartStore()
+import { ElMessage } from 'element-plus'
+import { useCartStore } from '@/stores/cartStore'
+const cartStore = useCartStore()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -24,31 +24,32 @@ const skuChange = (sku) => {
 }
 
 // count
-// const count = ref(1)
-// const countChange = (count) => {
-//   console.log(count)
-// }
+const count = ref(1)
+const countChange = (count) => {
+  console.log(count)
+}
 
 // 添加购物车
-// const addCart = () => {
-//   if (skuObj.skuId) {
-//     console.log(skuObj, cartStore.addCart)
-//     // 规则已经选择  触发action
-//     cartStore.addCart({
-//       id: goods.value.id,
-//       name: goods.value.name,
-//       picture: goods.value.mainPictures[0],
-//       price: goods.value.price,
-//       count: count.value,
-//       skuId: skuObj.skuId,
-//       attrsText: skuObj.specsText,
-//       selected: true
-//     })
-//   } else {
-//     // 规格没有选择 提示用户
-//     ElMessage.warning('请选择规格')
-//   }
-// }
+const addCart = () => {
+  if (skuObj.skuId) {
+    // skuObj.skuId作为判断依据
+    console.log(skuObj, cartStore.addCart)
+    // 规则已经选择  触发action
+    cartStore.addCart({
+      id: goods.value.id,//商品id
+      name: goods.value.name,//商品名称
+      picture: goods.value.mainPictures[0],//图片
+      price: goods.value.price,//最新价格
+      count: count.value,//商品数量
+      skuId: skuObj.skuId,//skuId
+      attrsText: skuObj.specsText,//商品规格文本
+      selected: true//商品是否选中
+    })
+  } else {
+    // 规格没有选择 提示用户
+    ElMessage.warning('请选择规格')
+  }
+}
 
 </script>
 
