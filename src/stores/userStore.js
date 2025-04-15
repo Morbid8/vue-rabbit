e7c11a85631091a74f07d2153a23fc370e3bd3dc
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-15 18:55:43 +0800

    登录-pinia管理用户数据

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,10 +1,10 @@
-// 管理用户数据相关
+// 利用pinia管理用户数据相关
 
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
-import { loginAPI } from '@/apis/user'
-import { useCartStore } from './cartStore'
-import { mergeCartAPI } from '@/apis/cart'
+// import { ref } from 'vue'
+// import { loginAPI } from '@/apis/user'
+// import { useCartStore } from './cartStore'
+// import { mergeCartAPI } from '@/apis/cart'
 export const useUserStore = defineStore('user', () => {
   const cartStore = useCartStore()
   // 1. 定义管理用户数据的state
@@ -14,27 +14,27 @@ export const useUserStore = defineStore('user', () => {
     const res = await loginAPI({ account, password })
     userInfo.value = res.result
     // 合并购物车的操作
-    await mergeCartAPI(cartStore.cartList.map(item => {
-      return {
-        skuId: item.skuId,
-        selected: item.selected,
-        count: item.count
-      }
-    }))
-    cartStore.updateNewList()
+    // await mergeCartAPI(cartStore.cartList.map(item => {
+    //   return {
+    //     skuId: item.skuId,
+    //     selected: item.selected,
+    //     count: item.count
+    //   }
+    // }))
+    // cartStore.updateNewList()
   }
 
-  // 退出时清除用户信息
-  const clearUserInfo = () => {
-    userInfo.value = {}
-    // 执行清除购物车的action
-    cartStore.clearCart()
-  }
+  // // 退出时清除用户信息
+  // const clearUserInfo = () => {
+  //   userInfo.value = {}
+  //   // 执行清除购物车的action
+  //   cartStore.clearCart()
+  // }
   // 3. 以对象的格式把state和action return
   return {
     userInfo,
     getUserInfo,
-    clearUserInfo
+    // clearUserInfo
   }
 }, {
   persist: true,
