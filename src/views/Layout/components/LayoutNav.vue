fed3fdd6122e52e0672d995e9be872e739e629d5
Morbid8 <morbid@stu2021.jnu.edu.cn>
2025-04-15 19:18:41 +0800

    登录-使用token适配多模板显示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -20,6 +20,7 @@ const confirm = () => {
         <!-- 多模版渲染 区分登录状态和非登录状态 -->
 
         <!-- 适配思路: 登录时显示第一块 非登录时显示第二块  是否有token -->
+         <!-- 根据token -->
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class=" iconfont icon-user"></i>{{ userStore.userInfo.account }}</a></li>
           <li>
